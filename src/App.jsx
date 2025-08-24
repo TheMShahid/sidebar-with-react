b5f3e159af2f6e7062d77252ea7e3dc8e469bc3b
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2025-08-24 20:53:45 +0500

    created the setup for sidebar project with react, and the two functionality open and close sidebar and modal

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./Home";
+import SideBar from "./Sidebar";
+import Modal from "./Modal";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <SideBar />
+      <Modal />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Sidebar.jsx
@@ -0,0 +1,9 @@
+const SideBar = () => {
+  return (
+    <div>
+      <h2>Side bar</h2>
+    </div>
+  );
+};
+
+export default SideBar;
